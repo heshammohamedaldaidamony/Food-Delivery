67ba524e3323e802dd818520386fff4bcaa5391f
Eslam Genedy <eslamgenedy377@yahoo.com>
2025-01-04 21:51:30 +0200

    add getter for restaurant

--- src/main/java/food_delivery/model/Restaurant.java
+++ src/main/java/food_delivery/model/Restaurant.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import java.io.Serializable;
 
@@ -14,6 +15,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Getter
 @Table(name = "restaurant")
 public class Restaurant implements Serializable {
     @Id
