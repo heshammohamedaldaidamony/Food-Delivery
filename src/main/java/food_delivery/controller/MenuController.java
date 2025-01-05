3378bf9a9d1607299d54549601079e04eaa60783
Eslam Genedy <eslamgenedy377@yahoo.com>
2025-01-05 10:09:56 +0200

    fix api path

--- src/main/java/food_delivery/controller/MenuController.java
+++ src/main/java/food_delivery/controller/MenuController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/menus")
+@RequestMapping("/api/v1/menu")
 public class MenuController {
 
     private final MenuService menuService;
