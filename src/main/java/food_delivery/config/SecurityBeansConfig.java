e2ec5ee1aefee6dd2ca0cea1caa4397476c7b395
Abdulhadi-assi <178458269+Abdulhadi-Assi@users.noreply.github.com>
2025-01-12 18:58:32 +0300

    adding user details and user details service

--- src/main/java/food_delivery/config/SecurityBeansConfig.java
+++ src/main/java/food_delivery/config/SecurityBeansConfig.java
@@ -1,13 +1,20 @@
 package food_delivery.config;
 
+import food_delivery.exception.ApplicationErrorEnum;
+import food_delivery.exception.BusinessException;
+import food_delivery.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityBeansConfig {
 
+    private final UserRepository userRepository;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -21,6 +28,11 @@ public class SecurityBeansConfig {
                         .allowCredentials(true);
             }
         };
+    }
+
+    public UserDetailsService userDetailsService()
+    {
+        return username -> userRepository.findByEmail(username).orElseThrow(() -> new BusinessException(ApplicationErrorEnum.USER_NOT_FOUND));
     }
 
 }
--- src/main/java/food_delivery/model/User.java
+++ src/main/java/food_delivery/model/User.java
@@ -2,10 +2,16 @@ package food_delivery.model;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
 @Data
@@ -13,7 +19,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Table(name = "\"user\"")
-public class User implements Serializable {
+public class User implements Serializable, UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -48,6 +54,43 @@ public class User implements Serializable {
 
     @Column(name = "verification_code")
     private String verificationCode;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    private List<UserRole> userRoles;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return userRoles
+                .stream()
+                .map(userRole -> new SimpleGrantedAuthority(userRole.getRole().getRoleName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return passwordHash;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
 
 
 }
